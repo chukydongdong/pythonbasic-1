175c35dd543e04b8487df57e648c13e512365224
chloe0330 <90747058+chloe0330@users.noreply.github.com>
2021-09-29 15:55:40 +0900

    Create main.py

---
+++ 2강/main.py
@@ -0,0 +1,90 @@
+\print('Hello, world!')print('Hello, world!')
+
+print("안녕하세요! 오늘은 '수요일'")
+print("월요일,화요일,수요일,목요일,금요일")
+a=10000
+b=25000
+c=30000
+d=a+b+c
+print(d)
+print(d/3)
+print("총금액:",d,"평균 금액:",d/3)
+print("""컴퓨터는 재밌고, 파이썬도 재밌다.""")
+print('''컴퓨터는 재밌고, 파이썬도 재밌다.''')
+print("\"파이썬은 쉽다\"라고\'철수\'가 말했어요")
+print("프로그래밍 언어는 다양하다.\n파이썬은 프로그래밍 언어이다.")
+print('''프로그래밍 언어는 다양하다.
+파이썬은 프로그래밍 언어이다.''')
+print("""프로그래밍 언어는 다양하다.
+파이썬은 프로그래밍 언어이다.""")
+string="""프로그래밍 언어는 다양하다.
+파이썬은 프로그래밍 언어이다."""
+print(string)
+print('''오늘 날씨는 이상하고
+	  나는 오늘 기분이 좋지 않다.''')
+head="파이썬은 "
+tail="재미있다"
+print(head+tail)
+head="오늘 날씨는 "
+tail="이상하다."
+print(head+tail)
+print("head"+"tail")
+a="파이썬"
+print(a*2)
+print("="*30)
+a="="
+a="여의도역은 여기에서 가깝다."
+print(len(a))
+string="""오늘 날씨는 이상하고
+나는 오늘 기분이 좋지 않다."""
+print(string)
+print(len(a))
+a= "life is too short, you need python"
+print(a[3])
+print(a[5])
+print(a[28])
+print('Hello, world!')
+
+print("안녕하세요! 오늘은 '수요일'")
+print("월요일,화요일,수요일,목요일,금요일")
+a=10000
+b=25000
+c=30000
+d=a+b+c
+print(d)
+print(d/3)
+print("총금액:",d,"평균 금액:",d/3)
+print("""컴퓨터는 재밌고, 파이썬도 재밌다.""")
+print('''컴퓨터는 재밌고, 파이썬도 재밌다.''')
+print("\"파이썬은 쉽다\"라고\'철수\'가 말했어요")
+print("프로그래밍 언어는 다양하다.\n파이썬은 프로그래밍 언어이다.")
+print('''프로그래밍 언어는 다양하다.
+파이썬은 프로그래밍 언어이다.''')
+print("""프로그래밍 언어는 다양하다.
+파이썬은 프로그래밍 언어이다.""")
+string="""프로그래밍 언어는 다양하다.
+파이썬은 프로그래밍 언어이다."""
+print(string)
+print('''오늘 날씨는 이상하고
+	  나는 오늘 기분이 좋지 않다.''')
+head="파이썬은 "
+tail="재미있다"
+print(head+tail)
+head="오늘 날씨는 "
+tail="이상하다."
+print(head+tail)
+print("head"+"tail")
+a="파이썬"
+print(a*2)
+print("="*30)
+a="="
+a="여의도역은 여기에서 가깝다."
+print(len(a))
+string="""오늘 날씨는 이상하고
+나는 오늘 기분이 좋지 않다."""
+print(string)
+print(len(a))
+a= "life is too short, you need python"
+print(a[3])
+print(a[5])
+print(a[28])
