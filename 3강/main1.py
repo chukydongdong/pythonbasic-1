2500f69a67d04fd466cd37713b61143b43704145
chloe0330 <90747058+chloe0330@users.noreply.github.com>
2021-10-06 15:52:34 +0900

    Create main1.py

---
+++ 3강/main1.py
@@ -0,0 +1,21 @@
+print('Hello, world!')
+age=12
+print(age)
+
+공책 = 3
+필통 = 1
+교과서 = 1
+print(공책,필통,교과서)
+string=('''오늘 날씨는 비가온다.
+		나는 오늘 덥다.''')
+print(string)
+hello=('''안녕하세요! 저는 12살 입니다.''')
+print(hello)
+head="오늘 날씨는 "
+tail="비가온다."
+print(head+tail)
+print("=" * 50)
+print("my program")
+print("=" * 50)
+hello = "안녕하세요! 오늘은 기분이 좋습니다."
+print(hello[5])
